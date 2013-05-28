1f6635314a205ec0d54f3a3d313034917c63e85f
Joshua Roberts <joshua.m.roberts@gmail.com>
2013-05-28 14:35:23 +1000

    split puppet module up with sub class and conditional to determine puppet master or client.

--- development/modules/puppet/manifests/init.pp
+++ development/modules/puppet/manifests/init.pp
@@ -5,34 +5,65 @@
 class puppet {
 
     $puppetmaster      = hiera('puppet_master','puppetmaster.example.com')
-    $puppet_packages   = hiera('puppet_packages','puppet')
-    $puppet_services   = hiera('puppet_services','puppet')
     $environment       = hiera('environment','production')
 
 #work out if puppet master or client based on puppet_role fact    
     if $::puppet_role == 'master' {
-      $puppet_template = 'puppet/puppet_server.conf.erb'
+      
+        include puppet::client
+    
     } else {
-      $puppet_template = 'puppet/puppet_client.conf.erb'
+        
+        include puppet::server
+    
     }
     
-    package { $puppet_packages:
-      ensure => present,
-      before => File[$puppet_template],
-    }
-
-    file { $puppet_template:
-      ensure => file,
-      mode   => 644,
-      content => template($puppet_template),
-    }
-
-    service { $puppet_services:
-      ensure     => running,
-      enable     => true,
-      hasrestart => true,
-      hasstatus  => true,
-      subscribe  => File[$puppet_template],
-    }
+}
+    
+class puppet::client {
+       
+  package { 'puppet':
+    ensure  => present,
+    before => File['/etc/puppet/puppet.conf'],
+  }
+        
+  file { '/etc/puppet/puppet.conf':
+    ensure => file,
+    mode   => 644,
+    content => template('puppet/puppet_client.conf.erb'),
+  }
+        
+  service { 'puppet':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    subscribe  => File['/etc/puppet/puppet.conf'],
+  }
 
 }
+      
+class puppet::server {
+        
+       
+  package { ['puppet','puppet-server']:
+    ensure  => present,
+    before => File['/etc/puppet/puppet.conf'],
+  }
+        
+  file { '/etc/puppet/puppet.conf':
+    ensure => file,
+    mode   => 644,
+    content => template('puppet/puppet_server.conf.erb'),
+  }
+        
+  service { ['puppet','puppetmaster']:
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    subscribe  => File['/etc/puppet/puppet.conf'],
+  }
+      
+}
+
