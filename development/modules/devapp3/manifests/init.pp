8df82c642c76aaff7dfb8d848a72efc95ccf9398
Joshua Roberts <joshua.m.roberts@gmail.com>
2013-05-28 16:34:26 +1000

    setup sample app hiera data

--- development/modules/devapp3/manifests/init.pp
+++ development/modules/devapp3/manifests/init.pp
@@ -6,5 +6,12 @@ class devapp3 {
       mode    => 0640,
       content => "I'm a DevApp3.",
     }
+    
+ file {'/tmp/devapp3_database.conf':
+      path    => '/tmp/devapp3_database.conf',
+      ensure  => present,
+      mode    => 0640,
+      content => template(devapp3_database.comf.erb),
+ }
 
 }
