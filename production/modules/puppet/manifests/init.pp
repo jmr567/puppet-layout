0725b34d4f725819d60a080d552e4a16893d9023
Joshua Roberts <joshua.m.roberts@gmail.com>
2013-05-28 15:53:50 +1000

    moving code from development --> staging --> prod

--- production/modules/puppet/manifests/init.pp
+++ production/modules/puppet/manifests/init.pp
@@ -1,30 +1,68 @@
-# ssh.pp
-
-#include ssh
+# puppet.pp
 
 class puppet {
 
-    $puppetmaster      = hiera('puppetmaster')
-    $puppet_packages   = hiera('puppet_packages')
-    $puppet_services   = hiera('puppet_services')
+    $puppetmaster     = hiera('puppet_master','puppetmaster.example.com')
+    $environment      = hiera('environment','production')
+    $puppet_role      = hiera('puppet_role','client')
 
-    package { $puppet_packages:
-      ensure => present,
-      #before => File['/etc/ssh/sshd_config'],
+#work out if puppet master or client based on puppet_role fact    
+    if $puppet_role == 'master' {
+      
+        include puppet::server
+    
+    } else {
+        
+        include puppet::client
+    
     }
-
-    #file { '/etc/ssh/sshd_config':
-    #  ensure => file,
-    #  mode   => 600,
-    #  content => template('ssh/sshd_config.erb'),
-    #}
-
-    service { $puppet_services:
-      ensure     => running,
-      enable     => true,
-      hasrestart => true,
-      hasstatus  => true,
-      #subscribe  => File['/etc/ssh/sshd_config'],
-    }
+    
+}
+    
+class puppet::client {
+       
+  package { 'puppet':
+    ensure  => present,
+    before => File['/etc/puppet/puppet.conf'],
+  }
+        
+  file { '/etc/puppet/puppet.conf':
+    ensure => file,
+    mode   => 644,
+    content => template('puppet/puppet_client.conf.erb'),
+  }
+        
+  service { 'puppet':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    subscribe  => File['/etc/puppet/puppet.conf'],
+  }
 
 }
+      
+class puppet::server {
+        
+       
+  package { ['puppet','puppet-server']:
+    ensure  => present,
+    before => File['/etc/puppet/puppet.conf'],
+  }
+        
+  file { '/etc/puppet/puppet.conf':
+    ensure => file,
+    mode   => 644,
+    content => template('puppet/puppet_server.conf.erb'),
+  }
+        
+  service { ['puppet','puppetmaster']:
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    subscribe  => File['/etc/puppet/puppet.conf'],
+  }
+      
+}
+
--- production/modules/testhiera/manifests/init.pp
+++
@@ -1,6 +0,0 @@
-class testhiera {
-    $testmsg = hiera("testmsg")
-    file { '/tmp/testhiera.txt':
-        content => inline_template("<%= testmsg %>\n"),
-    }
-}
