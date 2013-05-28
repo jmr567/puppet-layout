b47e439bc7c058bb004f3aaff69d86676b852774
Joshua Roberts <joshua.m.roberts@gmail.com>
2013-05-28 15:25:53 +1000

    adding best practice facter 1.7 implementation for basic text based key value pairs

--- development/modules/facter/manifests/init.pp
+++ development/modules/facter/manifests/init.pp
@@ -1,7 +1,14 @@
 class facter {
   
-  package { 'facter':
-    ensure => latest,
+  package { "facter":
+    ensure => "latest",
+  }
+  
+  file { "/etc/facter/facts.d":
+    ensure => "directory",
+    owner => "root",
+    group => "root",
+    require => File["/etc/facter/facts.d"],
   }
   
 }
