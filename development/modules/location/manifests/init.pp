b79d8bace8ce37afbcc0a46479d951a6ba313c44
Joshua Roberts <joshua.m.roberts@gmail.com>
2013-05-30 11:09:25 +1000

    updated typo

--- development/modules/location/manifests/init.pp
+++ development/modules/location/manifests/init.pp
@@ -1,7 +1,7 @@
 class location {
     $testmsg = hiera("location","production")
     file { '/etc/location':
-        content => inline_template("Environemnt set by Hiera as <%= testmsg %>\n"),
+        content => inline_template("Environment set by Hiera as <%= testmsg %>!\n"),
 	ensure => present,
     }
 }
--- production/modules/location/manifests/init.pp
+++ production/modules/location/manifests/init.pp
@@ -1,7 +1,7 @@
 class location {
     $testmsg = hiera("location","production")
     file { '/etc/location':
-        content => inline_template("<%= testmsg %>\n"),
+        content => inline_template("Environment set by Hiera as <%= testmsg %>!\n"),
 	ensure => present,
     }
 }
