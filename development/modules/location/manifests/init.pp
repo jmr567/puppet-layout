4ad0aa915c149773d3eaf68542b5c51bf2151e26
Joshua Roberts <joshua.m.roberts@gmail.com>
2015-03-04 17:10:22 +1100

    Cleaning up

--- development/modules/location/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-class location {
-    $testmsg = hiera("location","production")
-    file { '/etc/location':
-        content => inline_template("Environment set by Hiera as <%= testmsg %>!\n"),
-	ensure => present,
-    }
-}
-
